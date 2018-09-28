f72a0331a18d27d3624171c8f528ca18c9374e67
Deepak Routray <droutray@cisco.com>
2018-09-28 14:05:46 +0000

    Reverted back the changes

--- data/run_me.sh
+++ data/run_me.sh
@@ -9,5 +9,5 @@ if [ ! -f ${BASE_DIR}/install.log ] ; then
 fi
 
 echo "Starting the server"
-sed -i "3i JAVA_OPTS=$JAVA_OPTS -server -Xmx$MAX_HEAP -XX:MetaspaceSize=256m -XX:MaxMetaspaceSize=256m  -Dcom.sun.identity.configuration.directory=${BASE_DIR}" ${CATALINA_HOME}/bin/catalina.sh
+sed -i '3i JAVA_OPTS="$JAVA_OPTS -server -Xmx$MAX_HEAP -XX:MetaspaceSize=256m -XX:MaxMetaspaceSize=256m  -Dcom.sun.identity.configuration.directory=${BASE_DIR}"' ${CATALINA_HOME}/bin/catalina.sh
 ${CATALINA_HOME}/bin/catalina.sh run
